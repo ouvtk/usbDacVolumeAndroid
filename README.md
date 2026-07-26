b3fd27bca1aecf51d199c65b583f4725e4a39f2e
Stepan Tsymbal <9898545+ouvtk@users.noreply.github.com>
2026-07-26 15:44:34 +1000

    Attempt to support multiple devices

--- README.md
+++ README.md
@@ -3,39 +3,11 @@
 Simple application to set the USB DAC volume on UNROOTED Android Devices
 
 ## This is a fork of the original app
-I improved it so it only open the app when apple dongle detected and have options for auto exit the app (thanks to [polkaulfield](https://github.com/polkaulfield/usbDacVolumeAndroid))
 
 # Why
 The vast majority of high end android smartphones sold today do not contain a 3.5mm headphone jack. To remedy this, most people will use a USB-C to 3.5mm DAC. However, there are certian DACs (namely the Apple USB- C DAC) that do not default to their highest output setting. On most platforms (Windows, Linux, macOS, iOS), this isn't an issue because they either force the highest DAC volume and adjust their own mixer volume, or they control the DAC volume explicitly. Android does neither, so as a result, some DACs are quieter than they possibly can be.
 
-## Tested Devices
- - OnePlus 12R/Android 14 (Working)
- - OnePlus 7T/Android10/12 (Working)
- - Pixel 3/Android 11 (Working)
- - Galaxy A34/Android 14 (Working)
- - Huawei P20/Android 10 (NOT WORKING... Unknown reason...)
- - Maybe more? Let me know.
-
-# Usage
-Launch the application, and connect your DAC. 
-You should recieve a prompt asking to give USB Permission to the application. Press OK.
-<img src="https://github.com/guyman624/usbDacVolumeAndroid/assets/82007920/48d92739-bc2a-406b-853c-a14bf6f1228a" width="512">
-
-Once you have accepted, you should see this:<br>
-<img src="https://github.com/guyman624/usbDacVolumeAndroid/assets/82007920/b9e5bdfe-7f91-4eb1-b846-b6d6fb4a7216" width="512">
-
-
-- The top text input is the HEXADECIMAL value to send to the dongle. Leave this as 0000 for 100% volume.
-
-- Auto Apply on Start means that the new value will be sent to the dongle immidiately upon detection.
-
-- Apply will set the volume on the device.
-
-- The bottom is the name of the detected USB Device.
-
-You will temporarily lose sound during the setting of volume, however you should be able to restart playback after it has beens set. Please be aware that this setting is only saved until you unplug the headphones.
-
-
 # Special thanks to:
 [ibaiGorordo](https://github.com/ibaiGorordo/libusbAndroidTest) for most of the code :>
 [polkaulfield](https://github.com/polkaulfield/usbDacVolumeAndroid) for auto exit code :>
+[100nandoo](https://github.com/100nandoo/usbDacVolumeAndroid)
--- app/src/main/java/com/example/libusbAndroidTest/MainActivity.java
+++ app/src/main/java/com/example/libusbAndroidTest/MainActivity.java
@@ -15,8 +15,11 @@ import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,11 +29,12 @@ import androidx.core.content.ContextCompat;
 
 import com.example.libusbAndroidTest.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'lib' library on application startup.
     static {
         System.loadLibrary("libusbAndroidTest");
     }
@@ -41,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvCurrentVolume;
     private TextView tvDeviceName;
     private EditText volInput;
+    private Spinner deviceSpinner;
 
     private CheckBox autoApply;
     private CheckBox quitAfterApply;
@@ -48,32 +53,30 @@ public class MainActivity extends AppCompatActivity {
     private int deviceDescriptor = -1;
     private static String deviceName;
 
-    // in Hex 0x5AC is apple vendor id
-    private static final String APPLE_VENDOR_ID = "1452";
+    // Store device mapping: display name -> UsbDevice
+    private HashMap<String, UsbDevice> deviceMap = new HashMap<>();
+    private List<String> deviceDisplayNames = new ArrayList<>();
 
-    // in Hex 0x110A is apple dongle product id
+    private static final String APPLE_VENDOR_ID = "1452";
     private static final String APPLE_DONGLE_PRODUCT_ID = "4362";
 
-
-    private static final String TAG = "USB DAC Volume Adjustment" ;
-    private static final String ACTION_USB_PERMISSION =
-            "com.android.example.USB_PERMISSION";
+    private static final String TAG = "USB DAC Volume Adjustment";
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
     private static final int RECORD_AUDIO_PERMISSION_CODE = 1;
 
     private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
-
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (ACTION_USB_PERMISSION.equals(action)) {
                 synchronized (this) {
-                    UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
 
                     if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-                        if(device != null){
-                            connectDevice(device);
+                        if (device != null) {
+                            // Refresh device list
+                            refreshDeviceList();
                         }
-                    }
-                    else {
+                    } else {
                         Log.d(TAG, "permission denied for device " + device);
                     }
                 }
@@ -81,28 +84,68 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private boolean isAppleDongle(UsbDevice device){
-        return device.getVendorId() == Integer.parseInt(APPLE_VENDOR_ID) && device.getProductId() == Integer.parseInt(APPLE_DONGLE_PRODUCT_ID);
+    private boolean isAppleDongle(UsbDevice device) {
+        return device.getVendorId() == Integer.parseInt(APPLE_VENDOR_ID) &&
+                device.getProductId() == Integer.parseInt(APPLE_DONGLE_PRODUCT_ID);
+    }
+
+    private String getDeviceDisplayName(UsbDevice device) {
+        String vendorId = "0x" + Integer.toHexString(device.getVendorId()).toUpperCase();
+        String productId = "0x" + Integer.toHexString(device.getProductId()).toUpperCase();
+
+        if (isAppleDongle(device)) {
+            return "Apple Dongle - " + device.getDeviceName();
+        }
+
+        return "Vendor: " + vendorId + " Product: " + productId + " (" + device.getDeviceName() + ")";
+    }
+
+    private void refreshDeviceList() {
+        deviceMap.clear();
+        deviceDisplayNames.clear();
+
+        PendingIntent permissionIntent = PendingIntent.getBroadcast(this, 0,
+                new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
+
+        HashMap<String, UsbDevice> deviceList = usbManager.getDeviceList();
+
+        for (UsbDevice device : deviceList.values()) {
+            String displayName = getDeviceDisplayName(device);
+            deviceMap.put(displayName, device);
+            deviceDisplayNames.add(displayName);
+
+            if (!usbManager.hasPermission(device)) {
+                usbManager.requestPermission(device, permissionIntent);
+            }
+        }
+
+        // Update spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, deviceDisplayNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        deviceSpinner.setAdapter(adapter);
+
+        if (deviceDisplayNames.size() > 0) {
+            tvDeviceName.setText("Devices found: " + deviceDisplayNames.size());
+        } else {
+            tvDeviceName.setText("No USB devices found");
+        }
     }
 
     private void refreshVolumeDisplay(int fileDescriptor) {
-        // call native code off UI thread
         new Thread(() -> {
             try {
                 byte[] vol = getDeviceVolume(fileDescriptor); // native
-                // prepare display text
                 final String text;
                 if (vol == null || vol.length < 4) {
                     text = "Volume: unknown";
                 } else {
-                    // interpret as little-endian 16-bit values
                     int left = ((vol[1] & 0xFF) << 8) | (vol[0] & 0xFF);
                     int right = ((vol[3] & 0xFF) << 8) | (vol[2] & 0xFF);
                     String leftHex = String.format("%04X", left & 0xFFFF);
                     String rightHex = String.format("%04X", right & 0xFFFF);
                     text = "Left: 0x" + leftHex + " (" + left + ")  Right: 0x" + rightHex + " (" + right + ")";
                 }
-                // update UI on UI thread
                 runOnUiThread(() -> tvCurrentVolume.setText(text));
             } catch (Exception e) {
                 runOnUiThread(() -> tvCurrentVolume.setText("Volume read error"));
@@ -110,50 +153,38 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
-    protected void connectDevice(UsbDevice device)
-    {
-        Log.d("UsbDevice", "device: " + device.getDeviceName() + " " + device.getVendorId() + " " + device.getProductId());
+    protected void connectDevice(UsbDevice device) {
         Log.d("UsbDevice",
-                "class: " + device.getDeviceClass() + " " + device.getDeviceSubclass() + " " + device.getDeviceProtocol());
+                "device: " + device.getDeviceName() + " " + device.getVendorId() + " " + device.getProductId());
+        Log.d("UsbDevice", "class: " + device.getDeviceClass() + " " + device.getDeviceSubclass() + " "
+                + device.getDeviceProtocol());
         boolean isAppleDongle = isAppleDongle(device);
-
         String vendorId = "0x" + Integer.toHexString(device.getVendorId()).toUpperCase();
         String productId = "0x" + Integer.toHexString(device.getProductId()).toUpperCase();
-        String deviceVendorIdAndProductId = isAppleDongle ? "Apple Dongle Detected!" :
-               "vendorId: " + vendorId + " productId:" + productId;
+        String deviceVendorIdAndProductId = isAppleDongle ? "Apple Dongle Detected!"
+                : "vendorId: " + vendorId + " productId: " + productId;
         tvDeviceName.setText(deviceVendorIdAndProductId);
-        UsbInterface intf = device.getInterface(0);
-        UsbEndpoint endpoint = intf.getEndpoint(0);
-        UsbDeviceConnection connection = usbManager.openDevice(device);
-        connection.claimInterface(intf, true);
-        int fileDescriptor = connection.getFileDescriptor();
 
-        deviceName = initializeNativeDevice(fileDescriptor);
-        deviceDescriptor = fileDescriptor;
-        refreshVolumeDisplay(deviceDescriptor);
+        try {
+            UsbInterface intf = device.getInterface(0);
+            UsbEndpoint endpoint = intf.getEndpoint(0);
+            UsbDeviceConnection connection = usbManager.openDevice(device);
+            connection.claimInterface(intf, true);
+            int fileDescriptor = connection.getFileDescriptor();
 
-        if(autoApply.isChecked()){
-            setDeviceVolume(fileDescriptor);
-            if(quitAfterApply.isChecked()){
-                finishAndRemoveTask();
+            deviceName = initializeNativeDevice(fileDescriptor);
+            deviceDescriptor = fileDescriptor;
+            refreshVolumeDisplay(deviceDescriptor);
+
+            if (autoApply.isChecked()) {
+                setDeviceVolume(fileDescriptor);
+                if (quitAfterApply.isChecked()) {
+                    finishAndRemoveTask();
+                }
             }
-        }
-    }
-
-    protected void checkUsbDevices()
-    {
-        PendingIntent permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
-
-        HashMap<String, UsbDevice> deviceList = usbManager.getDeviceList();
-
-        for (UsbDevice device : deviceList.values()) {
-            if(usbManager.hasPermission(device))
-            {
-                connectDevice(device);
-            }
-            else {
-                usbManager.requestPermission(device, permissionIntent);
-            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error connecting device: " + e.getMessage());
+            tvDeviceName.setText("Error connecting to device");
         }
     }
 
@@ -166,6 +197,7 @@ public class MainActivity extends AppCompatActivity {
         tvCurrentVolume = binding.currentVolume;
         tvDeviceName = binding.deviceName;
         volInput = binding.volume;
+        deviceSpinner = binding.deviceSpinner;
         autoApply = binding.autoApply;
         quitAfterApply = binding.quitAfterApply;
 
@@ -173,6 +205,23 @@ public class MainActivity extends AppCompatActivity {
         volInput.setText(settings.getString("volume", "007f"));
         autoApply.setChecked(settings.getBoolean("autoApply", false));
         quitAfterApply.setChecked(settings.getBoolean("quitAfterApply", false));
+
+        // Setup device spinner listener
+        deviceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedDeviceName = (String) parent.getItemAtPosition(position);
+                UsbDevice selectedDevice = deviceMap.get(selectedDeviceName);
+                if (selectedDevice != null) {
+                    connectDevice(selectedDevice);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
 
         binding.refreshVolumeButton.setOnClickListener(v -> {
             if (deviceDescriptor >= 0) {
@@ -189,16 +238,17 @@ public class MainActivity extends AppCompatActivity {
         IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
         ContextCompat.registerReceiver(this, usbReceiver, filter, ContextCompat.RECEIVER_NOT_EXPORTED);
 
-        checkUsbDevices();
+        // Refresh device list on startup
+        refreshDeviceList();
 
         requestRecordAudioPermission();
     }
 
     private void requestRecordAudioPermission() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)
-                != android.content.pm.PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.RECORD_AUDIO) != android.content.pm.PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.RECORD_AUDIO},
+                    new String[] { android.Manifest.permission.RECORD_AUDIO },
                     RECORD_AUDIO_PERMISSION_CODE);
         }
     }
@@ -215,10 +265,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void applyButtonPressed(View view){
+    public void applyButtonPressed(View view) {
         String volume = volInput.getText().toString();
 
-        if(deviceDescriptor < 0){
+        if (deviceDescriptor < 0) {
             tvDeviceName.setBackgroundColor(Color.RED);
             return;
         }
@@ -226,27 +276,28 @@ public class MainActivity extends AppCompatActivity {
         try {
             setDeviceVolume(deviceDescriptor);
             volInput.setBackgroundColor(Color.TRANSPARENT);
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             volInput.setText("");
             volInput.setBackgroundColor(Color.RED);
             return;
         }
 
         SharedPreferences settings = getApplicationContext().getSharedPreferences("myPrefs", 0);
-        if(!settings.getString("volume", "").equals(volume)) {
+        if (!settings.getString("volume", "").equals(volume)) {
             SharedPreferences.Editor editor = settings.edit();
             editor.putString("volume", volume);
             editor.apply();
         }
     }
-    public void autoApplyCheckboxPressed(View view){
+
+    public void autoApplyCheckboxPressed(View view) {
         SharedPreferences settings = getApplicationContext().getSharedPreferences("myPrefs", 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putBoolean("autoApply", autoApply.isChecked());
         editor.apply();
     }
 
-    public void quitAfterApplyCheckboxPressed(View view){
+    public void quitAfterApplyCheckboxPressed(View view) {
         SharedPreferences settings = getApplicationContext().getSharedPreferences("myPrefs", 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putBoolean("quitAfterApply", quitAfterApply.isChecked());
@@ -258,7 +309,7 @@ public class MainActivity extends AppCompatActivity {
         byte[] data = new byte[len / 2];
         for (int i = 0; i < len; i += 2) {
             data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i+1), 16));
+                    + Character.digit(s.charAt(i + 1), 16));
         }
         return data;
     }
@@ -268,13 +319,15 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String initializeNativeDevice(int fileDescriptor);
+
     public native byte[] getDeviceVolume(int fileDescriptor);
+
     public native void setDeviceVolume(int fileDescriptor, byte[] volume);
 
-    public void setDeviceVolume(int fileDescriptor){
+    public void setDeviceVolume(int fileDescriptor) {
         String volume = volInput.getText().toString();
 
-        if(!volume.matches("[0-9A-Fa-f]{4}")){
+        if (!volume.matches("[0-9A-Fa-f]{4}")) {
             throw new IllegalArgumentException();
         }
 
